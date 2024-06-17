d5e1d0556f28667455fd057b0399184d1fd02d18
systeembeheeratabix <133881189+systeembeheeratabix@users.noreply.github.com>
2024-06-17 15:29:16 +0000

    Complete overhaul of install script (#3)
    * install.sh completely rewritten
    
    * install.sh now uses a Brewfile to install software in bulk
    
    This one is meant for non-dev laptops.
    
    * Clarify Brewfile file name for non-dev laptops
    
    * Create Brewfile-dev for dev laptops

--- install.sh
+++ install.sh
@@ -1,134 +1,215 @@
-#!/bin/sh
+#first install xcode in a terminal with "xcode-select --install" we can't do this in the script because the dialog won't make the script wait
+#same here: if it's an arm64 based laptop (M1, M2, etc) run this command "sudo softwareupdate --install-rosetta"
 
-# START PART 1
-# Homebrew Script for OSX
-# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
-xcode-select --install
-# END PART 1
+#to run the script easily from the terminal, type: "export DEVELOPER=true; curl https://raw.githubusercontent.com/atabix/macbook-install/main/install.sh | zsh"
 
-# PART 2
-sudo chown -R $(whoami) $(brew --prefix)/share/zsh $(brew --prefix)/share/zsh/site-functions
-chmod u+w $(brew --prefix)/share/zsh $(brew --prefix)/share/zsh/site-functions
-echo "Installing brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-logs
-touch $(brew --prefix)/.metadata_never_index
+#bundle repeatable commands into functions
+zsh_env_install() {
+    #starship for zsh
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+    #oh-my-zsh for zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
+    #fzf for zsh auto completion
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && cd $HOME/.fzf && ./install --bin --no-update-rc --no-bash --no-zsh --no-fish && cd || exit
+    #changing .zshrc
+    echo "# plugins" > $HOME/.zshrc
+    echo "plugins=(" >> $HOME/.zshrc
+    echo "docker" >> $HOME/.zshrc
+    echo "fzf-tab" >> $HOME/.zshrc
+    echo "git" >> $HOME/.zshrc
+    echo "zsh-autosuggestions" >> $HOME/.zshrc
+    echo "zsh-history-substring-search" >> $HOME/.zshrc
+    echo "zsh-syntax-highlighting" >> $HOME/.zshrc
+    echo ")" >> $HOME/.zshrc
+    echo "# omz" >> $HOME/.zshrc
+    echo 'export ZSH="$HOME/.oh-my-zsh"' >> $HOME/.zshrc
+    echo 'source $ZSH/oh-my-zsh.sh' >> $HOME/.zshrc
+    echo "# alias" >> $HOME/.zshrc
+    echo 'alias ls="ls -hplav --color=always"' >> $HOME/.zshrc
+    echo "# env" >> $HOME/.zshrc
+    echo 'export PATH="$PATH:$HOME/.fzf/bin"' >> $HOME/.zshrc
+    echo "# inits" >> $HOME/.zshrc
+}
+vscode_extensions() {
+    #install vscode extensions
+    code --install-extension alefragnani.project-manager
+    code --install-extension amiralizadeh9480.laravel-extra-intellisense
+    code --install-extension atabixsolutions.hephaestus
+    code --install-extension bmewburn.vscode-intelephense-client
+    code --install-extension bradlc.vscode-tailwindcss
+    code --install-extension calebporzio.better-phpunit
+    code --install-extension coenraads.disableligatures
+    code --install-extension fireyy.vscode-language-todo
+    code --install-extension ikappas.composer
+    code --install-extension jaguadoromero.vscode-php-create-class
+    code --install-extension jeff-hykin.better-syntax
+    code --install-extension junstyle.php-cs-fixer
+    code --install-extension ldd-vs-code.better-package-json
+    code --install-extension mehedidracula.php-namespace-resolver
+    code --install-extension mikestead.dotenv
+    code --install-extension mohamedbenhida.laravel-intellisense
+    code --install-extension mrmlnc.vscode-apache
+    code --install-extension mrmlnc.vscode-duplicate
+    code --install-extension steoates.autoimport
+    code --install-extension vscode-icons-team.vscode-icons
+    code --install-extension vue.volar
+    code --install-extension waderyan.gitblame
+    code --install-extension xdebug.php-debug
+}
+dev_extras() {
+    #install tools with brew
+    cd || exit
+    curl -O https://raw.githubusercontent.com/atabix/macbook-install/main/Brewfile-dev
+    mv Brewfile-dev Brewfile
+    brew bundle install
+    source $HOME/.zshrc
 
-# Wait for brew to be installed and add the brew command to be recognized in the shell
-echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.zprofile
-eval "$($(brew --prefix)/bin/brew shellenv)"
-# END PART 2
+    #install lfs for git
+    git lfs install
 
-###################################### NOTE #########################################
-# START PART 3 Note: Only start with this part when part 1 and 2 are completed!
+    #volta
+    curl https://get.volta.sh | bash
+    $HOME/.volta/bin/volta install node
+}
+brew_autoupdate() {
+    #make brew autoupdate
+    brew tap homebrew/autoupdate
+    mkdir -p $HOME/Library/LaunchAgents
+    source $HOME/.zshrc
+    brew autoupdate start 86400 --upgrade --greedy --cleanup --enable-notification
+}
 
-# Update Brew
-echo "Updating Brew, please wait..."
-brew update
-
-# Virtualization & Containerizing Tools
-echo "Installing virtualization & containerizing tools"
-brew install --cask docker
-
-# Dev Tools
-echo "Installing development tools"
-brew install --cask herd
-brew install --cask tower
-curl https://get.volta.sh | bash
-~/.volta/bin/volta install node
-brew install --cask phpstorm
-brew install --cask npm
-brew install --cask node
-
-# Create Dev folders & Never index
-mkdir -p ~/Development/http/app
-mkdir -p ~/Development/http/dev
-mkdir -p ~/Development/http/logs
-touch ~/Development/.metadata_never_index
-
-# PHP CS Fixer
-mkdir -p $(brew --prefix)/lib/php-cs-fixer
-composer require --dev --working-dir=$(brew --prefix)/lib/php-cs-fixer friendsofphp/php-cs-fixer
-ln -s $(brew --prefix)/lib/php-cs-fixer/vendor/bin/php-cs-fixer $(brew --prefix)/bin/php-cs-fixer
-curl https://raw.githubusercontent.com/atabix/macbook-install/main/assets/scripts/php-cs-fixer.dist.php > ~/Development/.php-cs-fixer.dist.php
-
-# Git Tools
-echo "Installing Git tools"
-brew install git git-lfs git-svn
-git lfs install
-
-# Database tools
-
-echo "Installing database tools"
-brew install --cask tableplus
-brew install --cask dbngin
-
-# Editors / Command line tools
-brew install --cask visual-studio-code
-brew install gnu-sed
-brew install zsh
-brew install --cask iterm2 
-
-# AWS Tools
-echo "Installing AWS tools"
-brew install aws-elasticbeanstalk
-brew install awscli 
-
-# Web Tools
-echo "Installing web tools"
-brew install --cask google-chrome
-brew install --cask postman
-
-# Programming languages
-brew install cocoapods
-
-echo "Installing Mobile tools"
-if [[ $(uname -m) == 'arm64' ]]; then
-    sudo softwareupdate --install-rosetta
+#detect if developer variable has been set
+if [ -z ${DEVELOPER+x} ]; then 
+    echo "Please choose if this laptop will be a developer laptop or not!"; exit;
 fi
-brew install --cask flutter
-brew install --cask android-studio
-brew install java
+if [ "$DEVELOPER" = true ]; then
+    echo Developer laptop
+    if [[ $(uname -m) == 'arm64' ]]; then
+        echo ARM detected
+        echo "Install step: 1.0"
+        zsh_env_install
 
-echo 'export PATH="$(brew --prefix)/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+        #brew noninteractive install
+        echo "Install step: 1.1"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Other apps
-brew install --cask spotify
-brew install --cask authy
-brew install --cask teamviewer
-brew install --cask raycast
-brew install --cask rectangle
-brew install --cask firefox
-brew install --cask adobe-acrobat-reader
-#optional brew install --cask adobe-creative-cloud
+        #add brew binary to zsh path
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zshrc
 
-# Auto upgrade brew
-mkdir -p /Users/$(whoami)/Library/LaunchAgents
-brew autoupdate start 86400 --upgrade --cleanup --enable-notification
+        #tell macos to not index these folders
+        [ ! -d "$HOME/Development" ] && mkdir -p $HOME/Development
+        touch $HOME/Development/.metadata_never_index
+        touch /opt/homebrew/.metadata_never_index
 
-# Check these step for step. Where some broken. INSTALL THIS PART AFTER VISUAL STUDIO CODE HAS BEEN INSTALLED!
+        echo "Install step: 1.2"
+        dev_extras
 
-# If `code` alias is missing you can add it to the path: https://code.visualstudio.com/docs/setup/mac
-code --install-extension mrmlnc.vscode-apache
-code --install-extension atabixsolutions.hephaestus
-code --install-extension steoates.autoimport
-code --install-extension ldd-vs-code.better-package-json
-code --install-extension calebporzio.better-phpunit
-code --install-extension jeff-hykin.better-syntax
-code --install-extension ikappas.composer
-code --install-extension coenraads.disableligatures
-code --install-extension mikestead.dotenv
-code --install-extension mrmlnc.vscode-duplicate
-code --install-extension waderyan.gitblame
-code --install-extension amiralizadeh9480.laravel-extra-intellisense
-code --install-extension mohamedbenhida.laravel-intellisense
-code --install-extension jaguadoromero.vscode-php-create-class
-code --install-extension junstyle.php-cs-fixer
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension mehedidracula.php-namespace-resolver
-code --install-extension xdebug.php-debug
-code --install-extension alefragnani.project-manager
-code --install-extension vue.volar
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension bradlc.vscode-tailwindcss
-code --install-extension fireyy.vscode-language-todo
+        #php cs fix
+        echo "Install step: 1.3"
+        mkdir -p /opt/homebrew/lib/php-cs-fixer
+        composer require --dev --working-dir=/opt/homebrew/lib/php-cs-fixer friendsofphp/php-cs-fixer
+        ln -s /opt/homebrew/lib/php-cs-fixer/vendor/bin/php-cs-fixer /opt/homebrew/bin/php-cs-fixer
+        curl https://raw.githubusercontent.com/atabix/macbook-install/main/assets/scripts/php-cs-fixer.dist.php > $HOME/Development/.php-cs-fixer.dist.php
+
+        echo "Install step: 1.4"
+        vscode_extensions
+
+        #make starship work
+        echo 'eval "$(starship init zsh)"' >> $HOME/.zshrc
+
+        echo "Install step: 1.5"
+        brew_autoupdate
+    fi
+    if [[ $(uname -m) == 'x86_64' ]]; then
+        echo x86_64 detected
+        echo "Install step: 1.0"
+        zsh_env_install
+        #fix /usr/local permissions after starship install, this is only needed on intel macs because intel homebrew installs to /usr/local
+        sudo chown -R $(whoami) /usr/local/*
+
+        #brew noninteractive install
+        echo "Install step: 1.1"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/usr/local/bin/brew shellenv)"
+
+        #add brew binary to zsh path
+        echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $HOME/.zshrc
+
+        #tell macos to not index these folders
+        [ ! -d "$HOME/Development" ] && mkdir -p $HOME/Development
+        touch $HOME/Development/.metadata_never_index
+
+        echo "Install step: 1.2"
+        dev_extras
+
+        #php cs fix
+        echo "Install step: 1.3"
+        mkdir -p $HOME/.php-cs-fixer
+        composer require --dev --working-dir=$HOME/.php-cs-fixer friendsofphp/php-cs-fixer
+        echo 'export PATH="$PATH:$HOME/.php-cs-fixer/vendor/bin' >> $HOME/.zshrc
+        curl https://raw.githubusercontent.com/atabix/macbook-install/main/assets/scripts/php-cs-fixer.dist.php > $HOME/Development/.php-cs-fixer.dist.php
+
+        echo "Install step: 1.4"
+        vscode_extensions
+
+        #make starship work
+        echo 'eval "$(starship init zsh)"' >> $HOME/.zshrc
+
+        echo "Install step: 1.5"
+        brew_autoupdate
+    fi
+fi
+if [ "$DEVELOPER" = false ]; then
+    echo Non-developer laptop
+    if [[ $(uname -m) == 'arm64' ]]; then
+        echo ARM detected
+        #brew noninteractive install
+        echo "Install step: 1.0"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+
+        #add brew binary to zsh path
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zshrc
+
+        #tell macos to not index these folders
+        touch /opt/homebrew/.metadata_never_index
+
+        #install tools with brew
+        echo "Install step: 1.1"
+        cd || exit
+        curl -O https://raw.githubusercontent.com/atabix/macbook-install/main/Brewfile-nondev
+        mv Brewfile-nondev Brewfile
+        brew bundle install
+        source $HOME/.zshrc
+
+        echo "Install step: 1.2"
+        brew_autoupdate
+    fi
+    if [[ $(uname -m) == 'x86_64' ]]; then
+        echo x86_64 detected
+        #brew noninteractive install
+        echo "Install step: 1.0"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/usr/local/bin/brew shellenv)"
+
+        #add brew binary to zsh path
+        echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $HOME/.zshrc
+
+        #install tools with brew
+        echo "Install step: 1.1"
+        cd || exit
+        curl -O https://raw.githubusercontent.com/atabix/macbook-install/main/Brewfile-nondev
+        mv Brewfile-nondev Brewfile
+        brew bundle install
+        source $HOME/.zshrc
+
+        echo "Install step: 1.2"
+        brew_autoupdate
+    fi
+fi
